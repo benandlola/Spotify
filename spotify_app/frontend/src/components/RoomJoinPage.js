5e2bad4f08bc0ef651c2f691638753ab1c91fa7f
Benedek Sandor <benedekgordonsandor@gmail.com>
2023-09-07 18:09:17 -0700

    create room page progress

--- spotify_app/frontend/src/components/RoomJoinPage.js
+++ spotify_app/frontend/src/components/RoomJoinPage.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
-import { Button, Grid, Typography, TextField, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel } from "@mui/material";
-import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
-    defaultVotes = 2
     constructor(props) {
         super(props);
     }
     render() {
-        return (<Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-                <Typography component="h4" variant="h4">
-                    Create a Room
-                </Typography>
-            </Grid>   
-        </Grid>);
+        return <p>This is the room join page</p>
     }
 }
